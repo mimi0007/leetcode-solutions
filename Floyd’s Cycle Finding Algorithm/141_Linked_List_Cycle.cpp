f2c1ca713e893c607d6be68d665a9aa5a2681e6f
mimi0007 <afsanaaziz.mimi@gmail.com>
2024-03-11 09:32:22 +0600

    [ADD] solution for cycle detection algorithm

---
+++ Floyd’s Cycle Finding Algorithm/141_Linked_List_Cycle.cpp
@@ -0,0 +1,27 @@
+//Problem Link: 
+//https://leetcode.com/problems/linked-list-cycle/?envType=daily-question&envId=2024-03-06
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast = head, *slow = head;
+
+        while(fast != NULL && fast->next != NULL) {
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if(fast == slow)
+                return true;
+        }
+
+        return false;
+    }
+};
